a61ff4c4cbfaff9e2e90023408b9cf8f70d189d8
emmanuelgyan-amoah <xgyanamo@mendelu.cz>
2025-01-04 15:39:57 +0100

    first commit

---
+++ TodoApp/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
